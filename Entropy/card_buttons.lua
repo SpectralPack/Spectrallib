a7f94aa7f80c292afe2e2e5d49dd5ca5a40b2e67
lord-ruby <ruby@crimsonfang.com>
2026-02-26 03:23:53 +0000

    fix card button stuff and add useable jokers

--- Entropy/card_buttons.lua
+++ Entropy/card_buttons.lua
@@ -302,3 +302,280 @@ function G.UIDEF.use_and_sell_buttons(card)
     end
     return abc
 end
+
+G.FUNCS.can_reserve_joker = function(e)
+    local c1 = e.config.ref_table
+    if
+        #G.jokers.cards
+        < G.jokers.config.card_limit + (Cryptid.safe_get(c1, "edition", "negative") and 1 or 0)
+    then
+        e.config.colour = G.C.GREEN
+        e.config.button = "reserve_joker"
+    else
+        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+        e.config.button = nil
+    end
+end
+G.FUNCS.reserve_joker = function(e)
+    local c1 = e.config.ref_table
+    G.E_MANAGER:add_event(Event({
+        trigger = "after",
+        delay = 0.1,
+        func = function()
+            local c2 = copy_card(c1, nil, nil, true, false)
+            c1:remove()
+            c2:add_to_deck()
+            G.jokers:emplace(c2)
+            SMODS.calculate_context({ pull_card = true, card = c1 })
+            return true
+        end,
+    }))
+end
+
+G.FUNCS.can_open_booster = function(e)
+    if
+        G.STATE == G.STATES.SELECTING_HAND or G.STATE == G.STATES.SHOP
+    then
+        e.config.colour = G.C.GREEN
+        e.config.button = "open_booster"
+    else
+        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+        e.config.button = nil
+    end
+end
+G.FUNCS.open_booster = function(e)
+    local c1 = e.config.ref_table
+    G.GAME.DefineBoosterState = G.STATE
+    delay(0.1)
+    local area = c1.area
+    if area == G.shop_vouchers then
+        G.GAME.current_round.voucher.spawn[c1.config.center_key] = nil
+    end
+    if c1.ability.booster_pos then G.GAME.current_round.used_packs[c1.ability.booster_pos] = 'USED' end
+    --draw_card(G.hand, G.play, 1, 'up', true, card, nil, true)
+    if not c1.from_tag then
+      G.GAME.round_scores.cards_purchased.amt = G.GAME.round_scores.cards_purchased.amt + 1
+    end
+    if c1.RPerkeoPack then
+        G.RPerkeoPack = true
+    end
+    if G.blind_select then
+        G.blind_select:remove()
+        G.blind_prompt_box:remove()
+    end
+    e.config.ref_table.cost = 0
+    e.config.ref_table:open()
+    if c1.ability.cry_multiuse and to_big(c1.ability.cry_multiuse) > to_big(1) then
+        local card = c1
+        card.ability.cry_multiuse = card.ability.cry_multiuse - 1
+        card.ability.extra_value = -1 * math.max(1, math.floor(card.cost/2))
+        card:set_cost()
+        delay(0.4)
+
+        -- i make my own card eval status text :D
+
+        card:juice_up()
+        play_sound('generic1')
+        attention_text({
+            text = format_ui_value(card.ability.cry_multiuse),
+            scale = 1.1,
+            hold = 0.6,
+            major = card,
+            backdrop_colour = G.C.SET[card.config.center.set],
+            align = 'bm',
+            offset = {x = 0, y = 0.2}
+        })
+        local c2 = copy_card(c1)
+        c2:add_to_deck()
+        area:emplace(c2)
+
+    end
+    --c1:remove()
+end
+
+G.FUNCS.can_open_voucher = function(e)
+    local c1 = e.config.ref_table
+    e.config.colour = G.C.GREEN
+    e.config.button = "open_voucher"
+end
+G.FUNCS.open_voucher = function(e)
+    local state = G.STATE
+    local c1 = e.config.ref_table
+    c1.cost = 0
+    local area = c1.area
+    c1:redeem()
+    c1:start_dissolve()
+    c1:remove()
+    if c1.ability.cry_multiuse and to_big(c1.ability.cry_multiuse) > to_big(1) then
+        local card = c1
+        card.ability.cry_multiuse = card.ability.cry_multiuse - 1
+        card.ability.extra_value = -1 * math.max(1, math.floor(card.cost/2))
+        card:set_cost()
+        delay(0.4)
+
+        -- i make my own card eval status text :D
+
+        card:juice_up()
+        play_sound('generic1')
+        attention_text({
+            text = format_ui_value(card.ability.cry_multiuse),
+            scale = 1.1,
+            hold = 0.6,
+            major = card,
+            backdrop_colour = G.C.SET[card.config.center.set],
+            align = 'bm',
+            offset = {x = 0, y = 0.2}
+        })
+        local c2 = copy_card(c1)
+        c2:add_to_deck()
+        area:emplace(c2)
+
+    end
+    G.STATE = state
+end
+
+G.FUNCS.can_reserve_booster = function(e)
+    local c1 = e.config.ref_table
+    if
+        G.consumeables.config.card_count
+        < G.consumeables.config.card_limit + (Cryptid.safe_get(c1, "edition", "negative") and 1 or 0)
+    then
+        e.config.colour = G.C.GREEN
+        e.config.button = "reserve_booster"
+    else
+        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+        e.config.button = nil
+    end
+end
+G.FUNCS.reserve_booster = function(e)
+    local c1 = e.config.ref_table
+    --G.GAME.DefineBoosterState = G.STATE
+    --c1:open()
+    G.pack_cards:remove_card(c1)
+    G.consumeables.cards[#G.consumeables.cards + 1] = c1
+    c1.area = G.consumeables
+    c1.parent = G.consumeables
+    c1.layered_parallax = G.consumeables.layered_parallax
+    G.consumeables:set_ranks()
+    G.consumeables:align_cards()
+
+    SMODS.calculate_context({ pull_card = true, card = c1 })
+    G.GAME.pack_choices = G.GAME.pack_choices - 1
+    if G.GAME.pack_choices <= 0 then
+        G.FUNCS.end_consumeable(nil, delay_fac)
+    end
+    if c1.ability.glitched_crown then
+        local center = G.P_CENTERS[c1.ability.glitched_crown[c1.glitched_index]]
+        c1:set_ability(center)
+        c1.ability.glitched_crown = nil
+    end
+    --c1:remove()
+end
+
+G.FUNCS.can_buy_deckorsleeve = function(e)
+    local c1 = e.config.ref_table
+    e.config.colour = G.C.GREEN
+    e.config.button = "buy_deckorsleeve_2"
+end
+G.FUNCS.can_buy_deckorsleeve_from_shop = function(e)
+    local c1 = e.config.ref_table
+    if to_big(G.GAME.dollars+G.GAME.bankrupt_at) >= to_big(c1.cost) or Entropy.has_rune("rune_entr_naudiz") then
+        e.config.colour = G.C.GREEN
+        e.config.button = "buy_deckorsleeve_from_shop"
+    else
+        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+        e.config.button = nil
+    end
+end
+G.FUNCS.buy_deckorsleeve_from_shop = function(e)
+    local c1 = e.config.ref_table
+    --G.GAME.DefineBoosterState = G.STATE
+    --c1:open()
+    ease_dollars(-c1.cost)
+    G.FUNCS.redeem_deckorsleeve(e)
+end
+
+G.FUNCS.buy_deckorsleeve_2 = function(e)
+    local c1 = e.config.ref_table
+    --G.GAME.DefineBoosterState = G.STATE
+    --c1:open()
+    G.FUNCS.redeem_deckorsleeve(e)
+end
+
+G.FUNCS.redeem_deckorsleeve = function(e)
+    G.E_MANAGER:add_event(Event{
+        trigger = "after",
+        func = function()
+
+            local area
+            if G.STATE == G.STATES.HAND_PLAYED then
+                if not G.redeemed_vouchers_during_hand then
+                    G.redeemed_vouchers_during_hand =
+                        CardArea(G.play.T.x, G.play.T.y, G.play.T.w, G.play.T.h, { type = "play", card_limit = 5 })
+                end
+                area = G.redeemed_vouchers_during_hand
+            else
+                area = G.play
+            end
+
+            local card = e.config.ref_table
+            if card.config.center.key == "j_joker" then
+                card:set_ability(G.P_CENTERS.b_red)
+            end
+            card.area:remove_card(card)
+            card:add_to_deck()
+            area:emplace(card)
+            card.cost = 0
+            card:redeem_deck()
+            return true
+        end
+    })
+end
+
+G.FUNCS.can_reserve_card_to_deck = function(e)
+    local c1 = e.config.ref_table
+    e.config.colour = G.C.GREEN
+    e.config.button = "reserve_card_to_deck"
+end
+G.FUNCS.reserve_card_to_deck = function(e)
+    local c1 = e.config.ref_table
+    G.E_MANAGER:add_event(Event({
+        trigger = "after",
+        delay = 0.1,
+        func = function()
+            local c2 = copy_card(c1, nil, nil, true, false)
+            c1:remove()
+            c2:add_to_deck()
+			table.insert(G.playing_cards, c2)
+			G.deck:emplace(c2)
+			playing_card_joker_effects({ c2 })
+            SMODS.calculate_context({ pull_card = true, card = c1 })
+            return true
+        end,
+    }))
+end
+
+G.FUNCS.can_use_joker = function(e)
+    local center = e.config.ref_table.config.center
+    if
+        center.can_use and center:can_use(e.config.ref_table) and not e.config.ref_table.debuff
+        and G.STATE ~= G.STATES.HAND_PLAYED and G.STATE ~= G.STATES.DRAW_TO_HAND and G.STATE ~= G.STATES.PLAY_TAROT
+        and not (((G.play and #G.play.cards > 0) or (G.CONTROLLER.locked) or (G.GAME.STOP_USE and G.GAME.STOP_USE > 0)))
+    then
+        e.config.colour = G.C.RED
+        e.config.button = "use_joker"
+    else
+        e.config.colour = G.C.UI.BACKGROUND_INACTIVE
+        e.config.button = nil
+    end
+end
+G.FUNCS.use_joker = function(e)
+    local int = G.TAROT_INTERRUPT
+    G.TAROT_INTERRUPT = true
+    local center = e.config.ref_table.config.center
+    if center.use then
+        center:use(e.config.ref_table)
+    end
+    e.config.ref_table:juice_up()
+    G.TAROT_INTERRUPT = int
+end
