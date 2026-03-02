bcd5abd36e626d7c95062df572ab7248b52bb323
Eris (borb) <remcosveen@gmail.com>
2026-03-02 15:08:13 +0100

    `instant` parameter for level_up_suit

--- Entropy/suit_levels.lua
+++ Entropy/suit_levels.lua
@@ -1,4 +1,4 @@
-function Spectrallib.level_suit(suit, card, amt, chips_override)
+function Spectrallib.level_suit(suit, card, amt, chips_override, instant)
     amt = amt or 1
     local used_consumable = copier or card
     if not G.GAME.SuitBuffs then G.GAME.SuitBuffs = {} end
@@ -7,10 +7,12 @@ function Spectrallib.level_suit(suit, card, amt, chips_override)
     end
     if not G.GAME.SuitBuffs[suit].chips then G.GAME.SuitBuffs[suit].chips = 0 end
     if not G.GAME.SuitBuffs[suit].level then G.GAME.SuitBuffs[suit].level = 1 end
-    update_hand_text(
-    { sound = "button", volume = 0.7, pitch = 0.8, delay = 0.3 },
-    { handname = localize(suit,'suits_plural'), chips = number_format(G.GAME.SuitBuffs[suit].chips), mult = "...", level = G.GAME.SuitBuffs[suit].level }
-    )
+    if not instant then
+        update_hand_text(
+        { sound = "button", volume = 0.7, pitch = 0.8, delay = 0.3 },
+        { handname = localize(suit,'suits_plural'), chips = number_format(G.GAME.SuitBuffs[suit].chips), mult = "...", level = G.GAME.SuitBuffs[suit].level }
+        )
+    end
     G.GAME.SuitBuffs[suit].chips = G.GAME.SuitBuffs[suit].chips + (chips_override or 10)*amt
     G.GAME.SuitBuffs[suit].level = G.GAME.SuitBuffs[suit].level + amt
     for i, v in ipairs(G.I.CARD) do
@@ -18,34 +20,36 @@ function Spectrallib.level_suit(suit, card, amt, chips_override)
             v.ability.suit_bonus = (v.ability.suit_bonus or 0) + (chips_override or 10)*amt
         end
     end
-    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('tarot1')
-            if card then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true 
-        end 
-    }))
-    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('tarot1')
-            if card then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true 
-        end 
-    }))
-    update_hand_text({ sound = "button", volume = 0.7, pitch = 0.9, delay = 0 }, { chips="+"..number_format((chips_override or 10)*amt), StatusText = true })
-    G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
-            play_sound('tarot1')
-            if card then card:juice_up(0.8, 0.5) end
-            G.TAROT_INTERRUPT_PULSE = nil
-            return true 
-        end 
-    }))
-    update_hand_text({ sound = "button", volume = 0.7, pitch = 0.9, delay = 0 }, { level = G.GAME.SuitBuffs[suit].level, chips=number_format(G.GAME.SuitBuffs[suit].chips) })
-    delay(1.3)
-    update_hand_text(
-    { sound = "button", volume = 0.7, pitch = 1.1, delay = 0 },
-    { mult = 0, chips = 0, handname = "", level = "" }
-    )
+    if not instant then
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
+                play_sound('tarot1')
+                if card then card:juice_up(0.8, 0.5) end
+                G.TAROT_INTERRUPT_PULSE = nil
+                return true 
+            end 
+        }))
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
+                play_sound('tarot1')
+                if card then card:juice_up(0.8, 0.5) end
+                G.TAROT_INTERRUPT_PULSE = nil
+                return true 
+            end 
+        }))
+        update_hand_text({ sound = "button", volume = 0.7, pitch = 0.9, delay = 0 }, { chips="+"..number_format((chips_override or 10)*amt), StatusText = true })
+        G.E_MANAGER:add_event(Event({trigger = 'after', delay = 0.9, func = function()
+                play_sound('tarot1')
+                if card then card:juice_up(0.8, 0.5) end
+                G.TAROT_INTERRUPT_PULSE = nil
+                return true 
+            end 
+        }))
+        update_hand_text({ sound = "button", volume = 0.7, pitch = 0.9, delay = 0 }, { level = G.GAME.SuitBuffs[suit].level, chips=number_format(G.GAME.SuitBuffs[suit].chips) })
+        delay(1.3)
+        update_hand_text(
+        { sound = "button", volume = 0.7, pitch = 1.1, delay = 0 },
+        { mult = 0, chips = 0, handname = "", level = "" }
+        )
+    end
 end
 
 local get_chips_ref = Card.get_chip_bonus
