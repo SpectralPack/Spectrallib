419f0247a6e776294b0497de86516b687f8e77e2
Eris (borb) <remcosveen@gmail.com>
2026-06-30 19:25:48 +0200

    im so full of attributes, yum

--- Spectrallib/attributes.lua
+++ Spectrallib/attributes.lua
@@ -1,3 +1,5 @@
+if not SMODS.Attribute then return end
+
 local attributes = {
     "echips", "emult", "hyperchips", "hypermult",
     "eqchips", "eqmult", "asc", "xasc", "easc", "hyperasc",
@@ -5,6 +7,8 @@ local attributes = {
     "multiuse", "ccd",
     "escore", "eblindsize", "hyperscore", "hyperblindsize",
     "asc_power", --for general interacting with ascension power, but NOT the scoring effect (e.g. exploit or sol from cryptid)
+    "suit_level", "rank_level", --just the hand_level attribute but for ranks/suits
+    "backs", "vouchers", "sleeves", --attributes for center types that dont have one in smods (for some reason voucher is also missing in smods)
 }
 
 for _, v in ipairs(attributes) do
