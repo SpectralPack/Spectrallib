87179cb0bb53ddae8959094e14bf1f96d813cc00
Eris (borb) <remcosveen@gmail.com>
2026-07-10 12:53:16 +0200

    what exactly was the thought process behind this

--- compat/entropy.lua
+++ compat/entropy.lua
@@ -7,7 +7,7 @@ Entropy.blind_is = Spectrallib.blind_is
 local splib_calcascmod_ref = Spectrallib.calculate_ascension_modification
 function Spectrallib.calculate_ascension_modification(args)
     for _,axeh in ipairs(SMODS.find_card('j_entr_axeh')) do
-        args.calc_args.amount = args.calc_args.amount(axeh.ability.asc_mod)
+        args.calc_args.amount = args.calc_args.amount * axeh.ability.asc_mod
     end
     return splib_calcascmod_ref(args)
 end
