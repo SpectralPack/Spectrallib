b25b2a3d9d3b216d8c327f13f5ab2bd790a233f2
Eris (borb) <remcosveen@gmail.com>
2026-07-10 18:23:14 +0200

    eris who had not thought any of that

--- Spectrallib/attributes.lua
+++ Spectrallib/attributes.lua
@@ -8,9 +8,16 @@ local attributes = {
     "escore", "eblindsize", "hyperscore", "hyperblindsize",
     "asc_power", --for general interacting with ascension power, but NOT the scoring effect (e.g. exploit or sol from cryptid)
     "suit_level", "rank_level", --just the hand_level attribute but for ranks/suits
-    "backs", "vouchers", "sleeves", --attributes for center types that dont have one in smods (for some reason voucher is also missing in smods)
+    "backs", "sleeves", --attributes for center types that dont have one in smods (for some reason voucher is also missing in smods)
 }
 
 for _, v in ipairs(attributes) do
     SMODS.Attribute { key = v }
 end
+
+SMODS.Attribute {
+    key = "vouchers",
+    keys = {
+        "tag_voucher",
+    }
+}
