0251dd4eeffb08ae122b377ade8595399529ba30
notmario <59957544+notmario@users.noreply.github.com>
2026-09-26 02:05:00 +0800

    consumeable retrigger tweaks

--- Spectrallib/context.lua
+++ Spectrallib/context.lua
@@ -17,23 +17,29 @@ if Overflow then
         local retrig_total = 0 --this is for future use in potentially bulk using for high retrigger amounts
         local retrig_effects = {}
         for i = 1, #effects do
-            local eff = Spectrallib.safe_get(effects, i, "jokers")
-            while eff do
-                if eff.repetitions then
-                    retrig_effects[#retrig_effects+1] = eff
-                    retrig_total = retrig_total + math.floor(math.max(0, eff.repetitions))
-                else
-                    sendWarnMessage("Found effect table during repetition check with no assigned repetitions", "Spectrallib")
+            for _, eff in pairs(effects[i]) do
+                while eff do
+                    if eff.repetitions then
+                        retrig_effects[#retrig_effects + 1] = eff
+                        retrig_total = retrig_total + math.floor(math.max(0, eff.repetitions))
+                    else
+                        sendWarnMessage("Found effect table during repetition check with no assigned repetitions", "Spectrallib")
+                    end
+                    eff = eff.extra
                 end
-                eff = eff.extra
             end
         end
         --two loops for reason mentioned above
         for _, eff in ipairs(retrig_effects) do
-            for _ = 1, math.floor(eff.repetitions or 0) do
-                if not card:can_use_consumeable(true, true) then return end --if the consumable can no longer be used, abort
-                if not eff.no_message then card_eval_status_text(eff.message_card or eff.card or card, "extra", nil, nil, nil, { message = eff.message or localize("k_again_ex"), colour = eff.colour or G.C.FILTER }) end
-                use_ref(card, area)
+            if bulk_use_ref and eff.bulk_use_threshold and (eff.repetitions or 0) >= eff.bulk_use_threshold and Overflow.can_bulk_use(card) then
+                if not eff.no_message then card_eval_status_text(eff.message_card or eff.card or card, "extra", nil, nil, nil, { message = eff.bulk_use_message or eff.message or localize("k_again_ex"), colour = eff.colour or G.C.FILTER }) end
+                bulk_use_ref(card, area, eff.reptitions)
+            else
+                for _ = 1, math.floor(eff.repetitions or 0) do
+                    if not card:can_use_consumeable(true, true) then return end --if the consumable can no longer be used, abort
+                    if not eff.no_message then card_eval_status_text(eff.message_card or eff.card or card, "extra", nil, nil, nil, { message = eff.message or localize("k_again_ex"), colour = eff.colour or G.C.FILTER }) end
+                    use_ref(card, area)
+                end
             end
         end
     end
@@ -50,23 +56,30 @@ function Card:use_consumeable(area, copier)
     local retrig_total = 0 --this is for future use in potentially bulk using for high retrigger amounts
     local retrig_effects = {}
     for i = 1, #effects do
-        local eff = Spectrallib.safe_get(effects, i, "jokers")
-        while eff do
-            if eff.repetitions then
-                retrig_effects[#retrig_effects + 1] = eff
-                retrig_total = retrig_total + math.floor(math.max(0, eff.repetitions))
-            else
-                sendWarnMessage("Found effect table during repetition check with no assigned repetitions", "Spectrallib")
+        for _, eff in pairs(effects[i]) do
+            while eff do
+                if eff.repetitions then
+                    retrig_effects[#retrig_effects + 1] = eff
+                    retrig_total = retrig_total + math.floor(math.max(0, eff.repetitions))
+                else
+                    sendWarnMessage("Found effect table during repetition check with no assigned repetitions", "Spectrallib")
+                end
+                eff = eff.extra
             end
-            eff = eff.extra
         end
     end
+    print(retrig_total)
     --two loops for reason mentioned above
     for _, eff in ipairs(retrig_effects) do
-        for _ = 1, math.floor(eff.repetitions or 0) do
-            if not self:can_use_consumeable(true, true) then return end --if the consumable can no longer be used, abort
-            if not eff.no_message then card_eval_status_text(eff.message_card or eff.card or self, "extra", nil, nil, nil, { message = eff.message or localize("k_again_ex"), colour = eff.colour or G.C.FILTER }) end
-            use_ref(self, area)
+        if bulk_use_ref and eff.bulk_use_threshold and (eff.repetitions or 0) >= eff.bulk_use_threshold and Overflow.can_bulk_use(self) then
+            if not eff.no_message then card_eval_status_text(eff.message_card or eff.card or self, "extra", nil, nil, nil, { message = eff.bulk_use_message or eff.message or localize("k_again_ex"), colour = eff.colour or G.C.FILTER }) end
+            bulk_use_ref(self, area, eff.reptitions)
+        else
+            for _ = 1, math.floor(eff.repetitions or 0) do
+                if not self:can_use_consumeable(true, true) then return end --if the consumable can no longer be used, abort
+                if not eff.no_message then card_eval_status_text(eff.message_card or eff.card or self, "extra", nil, nil, nil, { message = eff.message or localize("k_again_ex"), colour = eff.colour or G.C.FILTER }) end
+                use_ref(self, area)
+            end
         end
     end
 end
